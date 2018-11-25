88f00d57c9d8e9a47d0d15b0061a4249e248790f
theist303 <45217431+theist303@users.noreply.github.com>
2018-11-25 14:13:23 +0100

    Add files via upload

---
+++ grade calculator.py
@@ -0,0 +1,27 @@
+#GRADE CALCULATOR
+#ENTER YOUR SCORE
+
+score=66
+
+if score>=70 and score<=100:
+    print ("Excellent:A")
+elif score>=60 and score<=69:
+    print ("Very Good:B")
+elif score>=50 and score<=59:
+    print ("Good:C")
+elif score>=45 and score<=49:
+    print ("Fair:D")
+elif score>=40 and score<=44:
+    print ("Pass:E")
+elif score==0 and score<=39:
+    print ("Fail:F")
+else:
+    print('please input any score within 0 and 100')
+
+
+    
+
+                             
+            
+                           
+                    
